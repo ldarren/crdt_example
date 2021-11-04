a6841cd25800a1b3e794ffec14a185ae88b0f56d
Darren Liew <ldarren@gmail.com>
2021-11-05 04:06:42 +0800

    better mapping ele to char

--- client.js
+++ client.js
@@ -1,10 +1,8 @@
 const {
 	EditorView,
 	EditorState,
-	keymap,
-	ChangeSet,
-	lineNumbers,
-	indentWithTab,
+	basicSetup,
+	liteSetup,
 	javascript
 } = cm
 
@@ -15,15 +13,15 @@ function Client(id, state, server){
 	this.view = new EditorView({
 		state: EditorState.create({
 			extensions: [
-				lineNumbers(),
-//				javascript(),
+				//basicSetup,
+				liteSetup,
+				//javascript(),
 				EditorView.updateListener.of(v => {
 					if (v.transactions.some(t => t.isUserEvent('am'))) return
 					const changes = this.cm2am(v)
 					if (!changes) return
 					server.update(changes)
 				}),
-//				keymap.of([indentWithTab]),
 			]
 		}),
 		parent: document.getElementById(id)
@@ -31,6 +29,55 @@ function Client(id, state, server){
 }
 
 Client.prototype = {
+	index2At(text, index, deleteCount, insertText){
+		const updates = []
+		let val
+		let insertAt = 0	
+		for (val of text) {
+			index -= val.length
+			if (index < 0) break
+			insertAt++
+			if (!index) break
+		}
+		if (deleteCount){
+			if (index){
+				updates.push({insertAt, deleteCount: 1})
+				if (deleteCount != val.length) updates.push({insertAt, insertText: val.slice(0, val.length + index)})
+			}else{
+				updates.push({insertAt, deleteCount: 1})
+				if (deleteCount != val.length) updates.push({insertAt, insertText: val.slice(val.length + index)})
+			}
+		}
+		if (insertText){
+			if (index){
+				updates.push({insertAt, deleteCount: 1})
+				updates.push({insertAt: insertAt, insertText: val.slice(0, val.length + index) + insertText + val.slice(val.length + index)})
+			}else{
+				updates.push({insertAt, insertText})
+			}
+		}
+		return updates
+	},
+	at2Index(text, at){
+		let index = 0
+		for (val of text) {
+			if (!at) break
+			index += val.length
+			at--
+		}
+		return index
+	},
+	count2Char(text, at, count){
+		let chr = 0
+		for (val of text) {
+			if (!at) chr += val.length
+			if (!count) break
+			if (at) at--
+			else count--
+		}
+		return chr
+	},
+
 	cm2am(v){
 		// validation
 		if (!v.docChanged) return
@@ -38,28 +85,34 @@ Client.prototype = {
 		if (!Array.isArray(arr)) return
 
 		let merge2 = Automerge.merge(Automerge.init(), this.merge)
-		let insertAt = 0
+		let index = 0
 		let replace = null
 		let insertText = null
+		let updates
 		// update AutoMerge
 		for (let i = 0, l = arr.length; i < l; ){
 			if (Array.isArray(arr[i])){
 				replace = arr[i++]
 			}else{
-				insertAt += arr[i++]
+				index += arr[i++]
 				replace = arr[i++]
 			}
 			if (!replace) break
 			const [deleteCount, ...insertTexts] = replace
 			insertText = insertTexts.join('\n')
 
-			merge2 = Automerge.change(merge2, doc => {
-				if (deleteCount) doc.text.deleteAt(insertAt, deleteCount)
-				if (insertText) doc.text.insertAt(insertAt, insertText)
-			})
-			console.log('cm2am merge2', insertAt, deleteCount, insertText, merge2.text.toString())
+			updates = this.index2At(merge2.text, index, deleteCount, insertText)
 
-			insertAt += insertText.length //- 1 // why need to ignore return?
+			console.log('cm2am merge2 before', index, deleteCount, insertText, merge2.text.toString())
+			merge2 = Automerge.change(merge2, doc => {
+				updates.forEach(u => {
+					if (u.deleteCount) doc.text.deleteAt(u.insertAt, u.deleteCount)
+					if (u.insertText) doc.text.insertAt(u.insertAt, u.insertText)
+				})
+			})
+			console.log('cm2am merge2 after', merge2.text.toString())
+
+			index += insertText.length
 		}
 
 		const changes = Automerge.getChanges(this.merge, merge2)
@@ -70,19 +123,18 @@ Client.prototype = {
 	am2cm(patch){
 		console.log('client receive ######', patch)
 		// create changes and sent to upstream
-		const [merge2, patch2] = Automerge.applyChanges(this.merge, patch)
-		this.merge = merge2
+		const [merge2, patch2] = Automerge.applyChanges(Automerge.merge(Automerge.init(), this.merge), patch)
 
 		const props = patch2.diffs.props
 		if (!props.text) return
 		console.log('am2cm merge2', props.text, merge2.text.toString())
 		const text = props.text
+		const cm = this.view
 		for (let key in text){
-			let offset = 0
 			let index
 			text[key].edits.forEach(diff => {
 				const changes = {}
-				index = diff.index //+ offset
+				index = this.at2Index(this.merge.text, diff.index)
 				switch (diff.action) {
 					case 'insert': {
 						changes.from = index
@@ -92,22 +144,22 @@ Client.prototype = {
 					}
 					case 'remove': {
 						changes.from = index
-						changes.to = index + diff.count
+						changes.to = index + this.count2Char(this.merge.text, index, diff.count)
 						changes.insert = ''
 						break
 					}
 				}
+				console.log('cm', changes, cm.state.doc.toString())
 				// update codemirror
-				this.view.dispatch(this.view.state.update({
+				cm.dispatch(cm.state.update({
 					changes,
-					effects: [],
-					filter: false,
+					//effects: [],
+					//filter: false,
 					remote: true,
 					userEvent: 'am'
 				}))
-
-				offset++ // put back the return key
 			})
 		}
+		this.merge = merge2
 	},
 }
--- editor.js
+++ editor.js
@@ -1,16 +1,55 @@
-import {lineNumbers} from '@codemirror/gutter'
-import {EditorView, keymap} from '@codemirror/view'
+import {EditorView, keymap, highlightSpecialChars, drawSelection, highlightActiveLine} from "@codemirror/view"
 import {EditorState, ChangeSet, Annotation, Transaction} from '@codemirror/state'
+
+import {basicSetup} from "@codemirror/basic-setup"
+import {history, historyKeymap} from "@codemirror/history"
+import {foldGutter, foldKeymap} from "@codemirror/fold"
+import {indentOnInput} from "@codemirror/language"
+import {lineNumbers, highlightActiveLineGutter} from "@codemirror/gutter"
+import {defaultKeymap,indentWithTab} from "@codemirror/commands"
+import {bracketMatching} from "@codemirror/matchbrackets"
+import {closeBrackets, closeBracketsKeymap} from "@codemirror/closebrackets"
+import {searchKeymap, highlightSelectionMatches} from "@codemirror/search"
+import {autocompletion, completionKeymap} from "@codemirror/autocomplete"
+import {commentKeymap} from "@codemirror/comment"
+import {rectangularSelection} from "@codemirror/rectangular-selection"
+import {defaultHighlightStyle} from "@codemirror/highlight"
+import {lintKeymap} from "@codemirror/lint"
+
 import {javascript} from '@codemirror/lang-javascript'
-import {indentWithTab} from '@codemirror/commands'
 
 window.cm = {
 	EditorView,
 	EditorState,
-	keymap,
-	ChangeSet,
-	lineNumbers,
-	indentWithTab,
+	basicSetup,
+	liteSetup: [
+		lineNumbers(),
+		highlightActiveLineGutter(),
+		highlightSpecialChars(),
+		history(),
+		foldGutter(),
+		drawSelection(),
+		EditorState.allowMultipleSelections.of(true),
+		indentOnInput(),
+		defaultHighlightStyle.fallback,
+		bracketMatching(),
+		closeBrackets(),
+		autocompletion(),
+		rectangularSelection(),
+		highlightActiveLine(),
+		highlightSelectionMatches(),
+		keymap.of([
+			...closeBracketsKeymap,
+			...defaultKeymap,
+			...searchKeymap,
+			...historyKeymap,
+			...foldKeymap,
+			...commentKeymap,
+			...completionKeymap,
+			...lintKeymap,
+			indentWithTab,
+		])
+	],
 	javascript
 }
 
