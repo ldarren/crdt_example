952f80ae69eac89c63b1ea7e2dc9d2d9c66c8821
Darren Liew <ldarren@gmail.com>
2021-11-12 02:51:23 +0800

    Mix datatypes (#2)
    * ver bump
    
    * added form
    
    * added api form
    
    * added obj sync
    
    * fixed obj sync

--- client.js
+++ client.js
@@ -79,12 +79,89 @@ function count2Char(text, at, count){
 	return chr
 }
 
+function form2Obj(form){
+	const obj = {}
+	for(let i = 0, l = form.length, input; i < l; i++){
+		input = form[i]
+		obj[input.name] = input.value
+		console.log(input.name, input.value)
+	}
+	const arr = []
+	if (obj.h1) arr.push(obj.h1)
+	if (obj.h2){
+		if (obj.r1 >= obj.r2) arr.push(obj.h2)	
+		else arr.unshift(obj.h2)	
+	}
+	obj.headers = arr
+	return obj
+}
+
+function submit(evt){
+	const form = evt.target.closest('form')
+	const obj = form2Obj(form)
+	const headers = obj.headers.reduce((acc, o) => {
+		Object.assign(acc, JSON.parse(o))
+		return acc
+	}, {})
+
+	const req = form.querySelector('div.editor .cm-content').textContent
+	__.ajax(obj.method, obj.url, req, {headers}, (err, state, xhr) => {
+		if (4 > state) return
+		form[form.length - 1].value = xhr
+	})
+}
+
+function blur(ctx, evt){
+	const form = evt.target.closest('form')
+	const obj = form2Obj(form)
+
+	const merge2 = Automerge.change(ctx.merge, doc => {
+		doc.method = obj.method
+		doc.url = obj.url
+		obj.headers.forEach((h, i) => doc.headers[i] = h)
+	})
+
+	const changes = Automerge.getChanges(ctx.merge, merge2)
+	if (!changes.length) return
+	ctx.merge = merge2
+	ctx.server.update(changes)
+}
+
+function updateUI(form, merge){
+	for (let i = 0, l = form.length, input; i < l; i++){
+		input = form[i]
+		switch(input.name){
+		case 'method':
+			input.value = merge.method
+			break
+		case 'url':
+			input.value = merge.url
+			break
+		case 'h1':
+			input.value = merge.headers[0] || ''
+			break
+		case 'r1':
+			input.value = 0
+			break
+		case 'h2':
+			input.value = merge.headers[1] || ''
+			break
+		case 'r2':
+			input.value = 0
+			break
+		}
+	}
+}
+
 function Client(id, state, server){
+	this.id = id
 	this.server = server
 	const [merge] = Automerge.applyChanges(Automerge.init(), state)
 	this.merge = merge
+	const form =  document.querySelector(`form#${id}`)
 	this.view = new EditorView({
 		state: EditorState.create({
+			doc: this.merge.req.toString(),
 			extensions: [
 				//basicSetup,
 				liteSetup,
@@ -97,8 +174,14 @@ function Client(id, state, server){
 				}),
 			]
 		}),
-		parent: document.getElementById(id)
+		parent: form.querySelector(`div.editor`)
 	})
+	updateUI(form, this.merge)
+	form.querySelector(`input[type=button]`).addEventListener('click', submit)
+	form.addEventListener('focusout', (evt) => {
+		blur(this, evt)
+	})
+	this.form = form
 }
 
 Client.prototype = {
@@ -126,16 +209,16 @@ Client.prototype = {
 			const [deleteCount, ...insertTexts] = replace
 			insertText = insertTexts.join('\n')
 
-			updates = index2At(merge2.text, index, deleteCount, insertText)
+			updates = index2At(merge2.req, index, deleteCount, insertText)
 
-			console.log('cm2am merge2 before', index, deleteCount, insertText, merge2.text.toString())
+			console.log('cm2am merge2 before', index, deleteCount, insertText, merge2.req.toString())
 			merge2 = Automerge.change(merge2, doc => {
 				updates.forEach(u => {
-					if (u.deleteCount) doc.text.deleteAt(u.insertAt, u.deleteCount)
-					if (u.insertText) doc.text.insertAt(u.insertAt, u.insertText)
+					if (u.deleteCount) doc.req.deleteAt(u.insertAt, u.deleteCount)
+					if (u.insertText) doc.req.insertAt(u.insertAt, u.insertText)
 				})
 			})
-			console.log('cm2am merge2 after', merge2.text.toString())
+			console.log('cm2am merge2 after', merge2.req.toString())
 
 			index += insertText.length
 		}
@@ -146,20 +229,24 @@ Client.prototype = {
 	},
 
 	am2cm(patch){
-		console.log('client receive ######', patch)
+		console.log('client receive ######', this.merge, patch)
 		// create changes and sent to upstream
 		const [merge2, patch2] = Automerge.applyChanges(Automerge.merge(Automerge.init(), this.merge), patch)
+		const merge = this.merge
+		this.merge = merge2
+
+		console.log('am2cm merge2', merge2, patch2)
+		updateUI(this.form, merge2)
 
 		const props = patch2.diffs.props
-		if (!props.text) return
-		console.log('am2cm merge2', props.text, merge2.text.toString())
-		const text = props.text
+		if (!props.req) return
+		const text = props.req
 		const cm = this.view
 		for (let key in text){
 			let index
 			text[key].edits.forEach(diff => {
 				const changes = {}
-				index = at2Index(merge2.text, diff.index)
+				index = at2Index(merge2.req, diff.index)
 				switch (diff.action) {
 					case 'insert': {
 						changes.from = index
@@ -169,7 +256,7 @@ Client.prototype = {
 					}
 					case 'remove': {
 						changes.from = index
-						changes.to = index + count2Char(this.merge.text, diff.index, diff.count)
+						changes.to = index + count2Char(merge.req, diff.index, diff.count)
 						changes.insert = ''
 						break
 					}
@@ -185,7 +272,6 @@ Client.prototype = {
 				console.log('cm', changes, cm.state.doc.toString())
 			})
 		}
-		this.merge = merge2
 	},
 }
 
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 importScripts('./dist/automerge.js', './src/worker_module.js')
 	
-let merge = Automerge.change(Automerge.init(), doc => doc.text = new Automerge.Text())
+let merge = Automerge.change(Automerge.init(), doc => {
+	doc.method = 'POST'
+	doc.url = 'https://httpbin.org/post'
+	doc.headers = ['{"Content-Type":"application/json"}']
+	doc.req= new Automerge.Text()
+	doc.req.insertAt(0, '{"foo":"bar"}')
+})
     
 // worker code 
 WorkerModule.define({
@@ -10,8 +16,11 @@ WorkerModule.define({
 		// receive same changes from upstream
 		try{
 			const [merge2, patch] = Automerge.applyChanges(merge, changes)
-			console.log('server merge2', patch.diffs.props.text, merge2.text.toString())
-			WorkerModule.master.update(changes)
+			console.log('server merge2', patch.diffs.props, merge2.req.toString())
+			if (Object.keys(patch.diffs.props).length){
+				const update = Automerge.getChanges(merge, merge2)
+				WorkerModule.master.update(update)
+			}
 			merge = merge2
 		}catch(ex){
 			console.error(ex)
